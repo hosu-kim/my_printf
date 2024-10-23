3b07b0dbacb455357af3ca38d5c9258841fa0538
lake <145069282+hosu-kim@users.noreply.github.com>
2024-10-23 18:09:02 +0200

    Update libft.h

--- my_lib/libft.h
+++ my_lib/libft.h
@@ -19,7 +19,7 @@
 
 typedef struct s_list // define the struct
 {
-	void			*content; // points to any type of data
+	void		*content; // points to any type of data
 	struct s_list	*next;
 }	t_list; // define an alias
 
@@ -30,7 +30,7 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-char	*ft_itoa(int n);
+char		*ft_itoa(int n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
@@ -38,16 +38,16 @@ int		ft_toupper(int c);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t num, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest_str, const void *src_str, size_t n);
 void	*ft_memmove(void *dest_str, const void *src_str, size_t n_bytes);
 void	*ft_memset(void *ptr, int char_to_fill, size_t bytes_to_set);
 
 // Input/output
-int		ft_putchar_fd(char c, int fd);
+int	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-int		ft_putstr_fd(char *s, int fd);
+int	ft_putstr_fd(char *s, int fd);
 
 // String handling
 char	**ft_split(char const *s, char c);
@@ -57,9 +57,9 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-int		ft_strlen(const char *s);
+int	ft_strlen(const char *s);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -69,7 +69,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
+int	ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
